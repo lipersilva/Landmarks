6fd6102abd5292004d42e83cf333aecab39b8204
Filipe Rodrigues da Silva <lipe_rsilva@hotmail.com>
2024-08-03 10:12:42 -0300

    Creating and combining views; Building lists and navigation;

---
+++ Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,30 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Filipe Rodrigues da Silva on 01/08/24.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+	var landmark: Landmark
+    var body: some View {
+		HStack {
+			landmark.image
+				.resizable()
+				.frame(width: 50, height: 50)
+			Text(landmark.name)
+			Spacer()
+		}
+    }
+}
+
+#Preview {
+	Group {
+		LandmarkRow(landmark: landmarks[0])
+		LandmarkRow(landmark: landmarks[1])
+		LandmarkRow(landmark: landmarks[2])
+	}
+}
+
