741752796a211f3149c4ae353b4e23a306f1b521
Filipe Rodrigues da Silva <lipe_rsilva@hotmail.com>
2024-08-03 15:18:17 -0300

    Mark favorite landmarks

--- Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Views/LandmarkRow.swift
@@ -15,14 +15,20 @@ struct LandmarkRow: View {
 				.resizable()
 				.frame(width: 50, height: 50)
 			Text(landmark.name)
+			
 			Spacer()
+			
+			if landmark.isFavorite {
+				Image(systemName: "star.fill")
+					.foregroundColor(.yellow)
+			}
 		}
     }
 }
 
 #Preview {
 	Group {
-		LandmarkRow(landmark: landmarks[0])
+//		LandmarkRow(landmark: landmarks[0])
 		LandmarkRow(landmark: landmarks[1])
 		LandmarkRow(landmark: landmarks[2])
 	}
